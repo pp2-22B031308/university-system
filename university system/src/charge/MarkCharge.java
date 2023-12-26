0cac5629cf8d5de51a38d96769ee9be8b62ede9e
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-27 00:00:29 +0600

    Update and rename MarkService.java to MarkCharge.java

--- university system/src/charge/MarkCharge.java
+++ university system/src/charge/MarkCharge.java
@@ -1,21 +1,8 @@
-package service;
+package charge;
 
-import users.*;
-import additional.*;
-import comporators.*;
 import course.*;
-import database.*;
-import enums.*;
-import exceptions.*;
-import service.*;
-import system.*;
 
-public class MarkService {
-//	public static double getPrefinalResult(double att1, double att2){
-//		double prefMark = att1+att2;
-//		if(att1+att2<60)return 60;
-//		return prefMark;
-//	}
+public class MarkCharge {
 	public static String printAttestation(Mark mark) {
 		return "1st Attestation-"+mark.getFirstAttestation()+"\n2nd Attestation-"+mark.getSecondAttestation()
 				+ "\nExam:"+" Numeric Grade-"+mark.getNumericGrade()+" Grade-"+mark.getGrade();
