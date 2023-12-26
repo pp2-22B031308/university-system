2e6a030df924a4f23ecf710023ab37aa226b06c1
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-26 23:42:14 +0600

    Update Manager.java

--- university system/src/users/Manager.java
+++ university system/src/users/Manager.java
@@ -1,41 +1,31 @@
 package users;
-import users.*;
-import additional.*;
-import comporators.*;
-import course.*;
-import database.*;
+
 import enums.*;
-import exceptions.*;
-import service.*;
-import system.*;
-import java.time.LocalDate;
 
 public class Manager extends Employee {
-	private static final long serialVersionUID = 1L;
-	private ManagerType managerType;
-	public Manager() {
-		super();
-	}
-	
-	
-	public Manager(String id, String name, String surname, LocalDate birthDate, Gender gender, String adress, String email,
-			String contactNumber, String login, String password,LocalDate hireDate, ManagerType managerType) {
-		super(id, name, surname, birthDate, gender, adress, email, contactNumber, login, password, hireDate);
-		this.managerType = managerType;
-		// TODO Auto-generated constructor stub
-	}
-	public ManagerType getManagerType() {
-		return managerType;
-	}
+    private static final long serialVersionUID = 1L;
+    private ManagerType managerType;
 
-	public void setManagerType(ManagerType managerType) {
-		this.managerType = managerType;
-	}
-	
-	@Override
-	public String toString() {
-		return "Manager [managerType=" + managerType + ", toString()=" + super.toString() + "]";
-	}
+    public Manager() {
+        super();
+    }
 
-	
+    public Manager(String id, String name, String surname, Gender gender, String email,
+                   String login, String password, ManagerType managerType) {
+        super(id, name, surname, gender, email, login, password);
+        this.managerType = managerType;
+    }
+
+    public ManagerType getManagerType() {
+        return managerType;
+    }
+
+    public void setManagerType(ManagerType managerType) {
+        this.managerType = managerType;
+    }
+
+    @Override
+    public String toString() {
+        return "Manager [managerType=" + managerType + ", toString()=" + super.toString() + "]";
+    }
 }
