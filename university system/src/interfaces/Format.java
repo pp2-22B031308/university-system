34f9e708fb1c3f7b3d5a9359554994075584522c
Yerkezhan Kurtulush <95469009+krterke@users.noreply.github.com>
2023-12-26 12:47:47 +0600

    initial commit

---
+++ university system/src/interfaces/Format.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public interface Format {
+	public static String formatCitation(String title, String authors, String journal, int year, int pages) {
+		return authors;
+		
+	}
+}
