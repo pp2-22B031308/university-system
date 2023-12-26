71c5268189ccea7e567d623d1382d46caa153bdd
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-26 23:42:58 +0600

    Update GraduateStudent.java

--- university system/src/users/GraduateStudent.java
+++ university system/src/users/GraduateStudent.java
@@ -1,53 +1,61 @@
 package users;
-
-import java.time.LocalDate;
-
-import additional.Organization;
 import enums.*;
-import exceptions.*;
+import additional.*;
+
+import enums.Gender;
 
 public class GraduateStudent extends Student {
-	//fields
-	private static final long serialVersionUID = 1L;
-	private StudentType studentType;
-	private Researcher supervisor;
-	
-	//constructors
-	public GraduateStudent() {
-		super();
-	}
-	public GraduateStudent(String id, String name, String surname, LocalDate birthDate, Gender gender, String adress,
-			String email, String contactNumber, String login, String password, Faculty faculty, StudyDegree degree,
-			Organization organization) {
-		super(id, name, surname, birthDate, gender, adress, email, contactNumber, login, password, faculty, degree,
-				organization);
-		// TODO Auto-generated constructor stub
-	}
-	public GraduateStudent(StudentType studentType, Researcher supervisor) throws LowHIndexException {
-		if (supervisor.gethIndex() < 3) {
-            throw new LowHIndexException("Supervisor's h-index is less than 3");
-        }
-		this.studentType = studentType;
-		this.supervisor = supervisor;
-	}
-	
-	//getters and setters
-	public StudentType getStudentType() {
-		return studentType;
-	}
-	public void setStudentType(StudentType studentType) {
-		this.studentType = studentType;
-	}
-	public Researcher getSupervisor() {
-		return supervisor;
-	}
-	public void setSupervisor(Researcher supervisor) {
-		this.supervisor = supervisor;
-	}
-	
-	
-	
-	
-	
+    private static final long serialVersionUID = 123456789L;
+
+    private String thesisTopic;
+    private boolean thesisDefended;
+    private DiplomaProject diplomaProject;
+
+    public GraduateStudent() {
+        this.thesisDefended = false;
+    }
+
+    public GraduateStudent(String id, String name, String surname, Gender gender, String email, String login,
+            String password, Faculty faculty, StudyDegree degree, Organization organization,
+            String thesisTopic, boolean thesisDefended) {
+    	super(id, name, surname, gender, email, login, password, faculty, degree, organization);
+    	this.thesisTopic = thesisTopic;
+    	this.thesisDefended = thesisDefended;
+    }
+
+    public String getThesisTopic() {
+        return thesisTopic;
+    }
+
+    public void setThesisTopic(String thesisTopic) {
+        this.thesisTopic = thesisTopic;
+    }
+
+    public boolean isThesisDefended() {
+        return thesisDefended;
+    }
+
+    public void setThesisDefended(boolean thesisDefended) {
+        this.thesisDefended = thesisDefended;
+    }
+
+    public DiplomaProject getDiplomaProject() {
+        return diplomaProject;
+    }
+
+    public void setDiplomaProject(DiplomaProject diplomaProject) {
+        this.diplomaProject = diplomaProject;
+    }
+
+    @Override
+    public String toString() {
+        return "GraduateStudent [thesisTopic=" + thesisTopic + ", thesisDefended=" + thesisDefended
+                + ", " + super.toString() + "]";
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        return super.compareTo(other);
+    }
 
 }
