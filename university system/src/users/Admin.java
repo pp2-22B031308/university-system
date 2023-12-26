51880c296b6693c2296e4cc89b9ce0f10eeb20cd
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-26 23:44:38 +0600

    Update Admin.java

--- university system/src/users/Admin.java
+++ university system/src/users/Admin.java
@@ -3,19 +3,19 @@ package users;
 
 
 public final class Admin {
-	private static String login = "ADM123";
+	private static String login = "Admin";
 	private static String password = "123";
-	private static Admin SA;
+	private static Admin admin;
 	private Admin() {};
 	private Admin(String login, String password) {
 		Admin.login = login;
 		Admin.password = password;
 	}
 	
-	public static Admin getSuperAdmin() {
-		if (SA == null) 
-			SA = new Admin(login, password);
-		return SA;
+	public static Admin getAdmin() {
+		if (admin == null) 
+			admin = new Admin(login, password);
+		return admin;
 	}
 	public static String getPassword() {
 		return password;
